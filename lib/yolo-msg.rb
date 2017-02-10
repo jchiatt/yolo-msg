0516637b5c845ddbbd000aa1fb1dc5410e4b450d
J.C. Hiatt <jchiatt@me.com>
2017-02-09 21:11:32 -0600

    Initial commit

---
+++ lib/yolo-msg.rb
@@ -0,0 +1,31 @@
+class Yolo
+	def self.msg
+print <<"EOF";
+                                                                                   
+                                                                                   
+YYYYYYY       YYYYYYY     OOOOOOOOO     LLLLLLLLLLL                  OOOOOOOOO     
+Y:::::Y       Y:::::Y   OO:::::::::OO   L:::::::::L                OO:::::::::OO   
+Y:::::Y       Y:::::Y OO:::::::::::::OO L:::::::::L              OO:::::::::::::OO 
+Y::::::Y     Y::::::YO:::::::OOO:::::::OLL:::::::LL             O:::::::OOO:::::::O
+YYY:::::Y   Y:::::YYYO::::::O   O::::::O  L:::::L               O::::::O   O::::::O
+   Y:::::Y Y:::::Y   O:::::O     O:::::O  L:::::L               O:::::O     O:::::O
+    Y:::::Y:::::Y    O:::::O     O:::::O  L:::::L               O:::::O     O:::::O
+     Y:::::::::Y     O:::::O     O:::::O  L:::::L               O:::::O     O:::::O
+      Y:::::::Y      O:::::O     O:::::O  L:::::L               O:::::O     O:::::O
+       Y:::::Y       O:::::O     O:::::O  L:::::L               O:::::O     O:::::O
+       Y:::::Y       O:::::O     O:::::O  L:::::L               O:::::O     O:::::O
+       Y:::::Y       O::::::O   O::::::O  L:::::L         LLLLLLO::::::O   O::::::O
+       Y:::::Y       O:::::::OOO:::::::OLL:::::::LLLLLLLLL:::::LO:::::::OOO:::::::O
+    YYYY:::::YYYY     OO:::::::::::::OO L::::::::::::::::::::::L OO:::::::::::::OO 
+    Y:::::::::::Y       OO:::::::::OO   L::::::::::::::::::::::L   OO:::::::::OO   
+    YYYYYYYYYYYYY         OOOOOOOOO     LLLLLLLLLLLLLLLLLLLLLLLL     OOOOOOOOO     
+                                                                                   
+                                                                                   
+                                                                                   
+                                                                                   
+                                                                                   
+                                                                                   
+                                                                                   
+EOF
+	end
+end
---
+++ yolo-msg.gemspec
@@ -0,0 +1,13 @@
+Gem::Specification.new do |s|
+  s.name        = 'yolo-msg'
+  s.version     = '1.0.0'
+  s.date        = '2017-02-09'
+  s.summary     = "YOLO!"
+  s.description = "Add a nice reminder to your console messages."
+  s.authors     = ["J.C. Hiatt"]
+  s.email       = 'jc@builtbygood.co'
+  s.files       = ["lib/yolo-msg.rb"]
+  s.homepage    =
+    'http://rubygems.org/gems/yolo-msg'
+  s.license       = 'MIT'
+end
